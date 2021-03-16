f7195138d63761c68ba8ca7c066291f32c277efe
Ezequiel Ginsberg <ezequielginsberg@gmail.com>
2021-03-16 00:15:00 -0300

    Added EntityFramework

---
+++ Wallet/Wallet.API/Controllers/ExamplesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wallet.Data.Models;
+
+namespace Wallet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamplesController : ControllerBase
+    {
+        private readonly WALLETContext _context;
+
+        public ExamplesController(WALLETContext context)
+        {
+            _context = context;
+        }
+
+        // JUST AN EXAMPLE !! DELETE IT LATER
+        [HttpGet]
+        public IEnumerable<Users> Get()
+        {
+            return _context.Users.ToList();
+        }
+        // JUST AN EXAMPLE !! DELETE IT LATER
+
+    }
+}
--- Wallet/Wallet.API/Startup.cs
+++ Wallet/Wallet.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wallet.Data.Models;
 
 namespace Wallet.API
 {
@@ -32,6 +34,8 @@ namespace Wallet.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Wallet.API", Version = "v1" });
             });
+            services.AddDbContext<WALLETContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("WalletDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
