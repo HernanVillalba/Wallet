2a9b5ca7a7f021a4c33331d876cd196c03a7a5be
Ezequiel Ginsberg <ezequielginsberg@gmail.com>
2021-03-19 19:05:56 -0300

    added the create functionality over the fixed term deposits

---
+++ Wallet/Wallet.API/Models/FixedTermDepositModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wallet.API.Models
+{
+    public class FixedTermDepositModel
+    {
+        [Required]
+        public int AccountId { get; set; }
+        [Required]
+        public float Amount { get; set; }
+    }
+}
