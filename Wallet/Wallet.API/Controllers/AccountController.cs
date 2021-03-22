8256c16c0bc0975d9765fca2317b0a37d743dcda
Bruno Ferrari <bruno91f@gmail.com>
2021-03-22 20:04:28 -0300

    fix error

--- Wallet/Wallet.API/Controllers/AccountController.cs
+++ Wallet/Wallet.API/Controllers/AccountController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using Wallet.Business.Operations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
