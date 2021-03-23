0ef7c0bf60a01d6e412f09fec81a7e637427863a
Bruno Ferrari <bruno91f@gmail.com>
2021-03-23 15:14:45 -0300

    update account business with new structure

--- Wallet/Wallet.API/Controllers/AccountController.cs
+++ Wallet/Wallet.API/Controllers/AccountController.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Wallet.Data.Repositories.Interfaces;
-using Wallet.Entities;
-using Wallet.Data.Models;
+using Wallet.Business.Logic;
 
 namespace Wallet.API.Controllers
 {
@@ -15,13 +10,25 @@ namespace Wallet.API.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IAccountLogic _accountLogic;
+        private readonly IAccountBusiness _accountBusiness;
 
-        public AccountController(IUnitOfWork unitOfWork, IAccountLogic accountLogic)
+        public AccountController(IAccountBusiness accountBusiness)
         {
-            _unitOfWork = unitOfWork;
-            _accountLogic = accountLogic;
+            _accountBusiness= accountBusiness;
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult ListAccounts()
+        {
+            var id = int.Parse(User.Claims.First(i => i.Type == "UserId").Value);
+            try
+            {
+                return Ok(_accountBusiness.GetAccountsWithBalance(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
 
         [Authorize]
@@ -31,45 +38,14 @@ namespace Wallet.API.Controllers
             var id = int.Parse(User.Claims.First(i => i.Type == "UserId").Value);
             try
             {
-                var balances = _accountLogic.SelectBalances(id, "ARS", "USD");
-                BalanceModel balance = new BalanceModel()
-                {
-                    ArgBalance = balances[0],
-                    UsdBalance = balances[1]
-                };
-                return Ok(balance);
+                var balances = _accountBusiness.GetBalances(id);               
+                return Ok(balances);
             }
             catch (Exception ex)
             {
                 return BadRequest(new { message = ex.Message });
             }
-        }
-
-        [Authorize]
-        [HttpGet]
-        public IActionResult ListAccounts()
-        {
-            var id = int.Parse(User.Claims.First(i => i.Type == "UserId").Value);
-            var accounts = _unitOfWork.Accounts.GetUserAccounts(id);
-            List<AccountModel> acc = new List<AccountModel>();
-            try
-            {
-                foreach (Accounts a in accounts)
-                {
-                    acc.Add(new AccountModel
-                    {
-                        Id = a.Id,
-                        Currency = a.Currency,
-                        Balance = _unitOfWork.Accounts.GetAccountBalance(id, a.Currency)
-                    });
-                }
-                return Ok(acc);
-            }
-            catch(Exception ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-        }
+        }        
     }
 }
 
---
+++ Wallet/Wallet.Business/Logic/IAccountBusiness.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Wallet.Entities;
+
+namespace Wallet.Business.Logic
+{
+    public interface IAccountBusiness
+    {
+        BalanceModel GetBalances(int id);
+        List<AccountModel> GetAccountsWithBalance(int id);
+    }
+}
