9c43bdc8749cbf5167ac7c5265fd190a4fb1c7bd
Ezequiel Ginsberg <ezequielginsberg@gmail.com>
2021-03-24 21:15:29 -0300

    Fixed unsucessful merge

--- Wallet/Wallet.Business/Profiles/AutoMapperProfile.cs
+++ Wallet/Wallet.Business/Profiles/AutoMapperProfile.cs
@@ -11,7 +11,6 @@ namespace Wallet.Business.Profiles
             CreateMap<Users, RegisterModel>().ReverseMap();
             CreateMap<Users, LoginModel>().ReverseMap();
             CreateMap<Transactions, TransactionCreateModel>().ReverseMap();
-            CreateMap<Transactions, TransactionModel>().ReverseMap();
             CreateMap<FixedTermDeposit, FixedTermDepositCreateModel>().ReverseMap();
             CreateMap<FixedTermDeposit, FixedTermDepositModel>().ReverseMap();
             CreateMap<Transactions, TransactionFilterModel>().ReverseMap();
