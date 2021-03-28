e0d9f54b9e159a2865f7b0cd103c9b818b1aa148
Ezequiel Ginsberg <ezequielginsberg@gmail.com>
2021-03-28 02:45:51 -0300

    Added another DataAnnotation validation

--- Wallet/Wallet.Entities/FixedTermDepositCreateModel.cs
+++ Wallet/Wallet.Entities/FixedTermDepositCreateModel.cs
@@ -8,12 +8,12 @@ namespace Wallet.Entities
 {
     public class FixedTermDepositCreateModel
     {
-        [Required(ErrorMessage = "Se requiere un id de cuenta mayor a cero y menor a 2^31")]
-        [Range(1, int.MaxValue)]
+        [Required(ErrorMessage = "El campo 'AccountId' es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Se requiere un id de cuenta mayor a cero y menor a 2^31")]
         public int AccountId { get; set; }
 
-        [Required(ErrorMessage = "Se requiere un monto mayor a cero y menor a 17e307")]
-        [Range(double.Epsilon, double.MaxValue)]
+        [Required(ErrorMessage = "El campo 'Amount' es obligatorio")]
+        [Range(double.Epsilon, double.MaxValue, ErrorMessage = "Se requiere un monto mayor a cero y menor a 17e307")]
         public float Amount { get; set; }
     }
 }
