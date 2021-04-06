c2e530e531de01d3039a49c57d9f7362a20789a6
hernanVillalba <63215609+hernanVillalba@users.noreply.github.com>
2021-04-06 20:38:17 -0300

    Refunds - Repository and controller upload

---
+++ Wallet/Wallet.Data/Repositories/RefundRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using Wallet.Data.Models;
+using Wallet.Data.Repositories.Interfaces;
+
+namespace Wallet.Data.Repositories
+{
+    public class RefundRequestRepository : GenericRepository<RefundRequest>, IRefundRequestRepository
+    {
+        public RefundRequestRepository(WALLETContext context) : base(context)
+        {
+
+        }
+    }
+}
