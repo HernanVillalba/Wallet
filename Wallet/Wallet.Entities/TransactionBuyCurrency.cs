cab2698e8d90d1fe489183408375c171c9b7d5a6
Bruno Ferrari <bruno91f@gmail.com>
2021-04-06 15:26:05 -0300

    fix namings

--- Wallet/Wallet.Entities/TransactionBuyCurrency.cs
+++ Wallet/Wallet.Entities/TransactionBuyCurrency.cs
@@ -9,11 +9,6 @@ namespace Wallet.Entities
         [RegularExpression("Compra|Venta", ErrorMessage = "Solo se acepta Compra o Venta")]
         public string Type { get; set; }
 
-        //[Required(ErrorMessage = "Tipo de moneda requerido (ARS o USD)")]
-        //[MaxLength(3, ErrorMessage = "Máximo 3 carácteres")]
-        //[RegularExpression("USD|ARS|usd|ars", ErrorMessage = "Solo se permite ARS o USD")]
-        //public string Currency { get; set; }
-
         [Required(ErrorMessage = "Monto requerido")]
         [Range(0.01, double.MaxValue, ErrorMessage = "El campo {0} debe ser mayor a cero")]
         public double Amount { get; set; }
