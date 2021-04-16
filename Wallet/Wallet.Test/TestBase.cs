7ebb2fdeab49990433f82d850438d537a82898ce
Bruno Ferrari <bruno91f@gmail.com>
2021-04-16 16:52:54 -0300

    add register tests, add setup for tests

--- Wallet/Wallet.Test/TestBase.cs
+++ Wallet/Wallet.Test/TestBase.cs
@@ -1,28 +1,46 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Wallet.API.Controllers;
+using Wallet.Business.Logic;
 using Wallet.Business.Profiles;
 using Wallet.Data.Models;
 using Wallet.Data.Repositories;
 using Wallet.Data.Repositories.Interfaces;
+using Wallet.Entities;
 
 namespace Wallet.Test
 {
-     public class TestBase
+    public class TestBase
     {
-        public static readonly IMapper _mapper;
-        public static readonly IUnitOfWork _unitOfWork;
-
-        static TestBase()
+        protected readonly IMapper _mapper;
+        protected readonly IUnitOfWork _unitOfWork;
+        protected ClaimsIdentity _identity;
+        protected ClaimsPrincipal _user;
+        protected ControllerContext _controllerContext;
+        protected WALLETContext context;
+        public TestBase()
         {
+            //Set Mock identity
+            _identity = new ClaimsIdentity();
+            _user = new ClaimsPrincipal(_identity);
+            _controllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = _user } };
+            _identity.AddClaims(new[]
+            {
+                new Claim("UserId", "1"),
+            });
             // Set Database in memory
             var options = new DbContextOptionsBuilder<WALLETContext>().UseInMemoryDatabase(databaseName: "WalletDB").Options;
-            var context = new WALLETContext(options);
-
+            context = new WALLETContext(options);
+            DataInitializer.Initialize(context);
+            var c = context.Users.ToList();
             // Set Unit of Work
             _unitOfWork = new UnitOfWork(context);
 
@@ -32,7 +50,14 @@ namespace Wallet.Test
                 mc.AddProfile(new AutoMapperProfile());
             });
             _mapper = mappingConfig.CreateMapper();
-	    }
-        
+        }
+        public List<Users> lista()
+        {
+            return context.Users.ToList();
+        }
+        ~TestBase()
+        {
+            context.Database.EnsureDeleted();
+        }
     }
 }
---
+++ Wallet/Wallet.Test/UserControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Wallet.API.Controllers;
+using Wallet.Business;
+using Wallet.Business.Logic;
+using Wallet.Entities;
+using Xunit;
+
+namespace Wallet.Test
+{
+    public class UserControllerTest : TestBase
+    {
+        private readonly UsersController usersController;
+        //Arrange for register
+        private readonly RegisterModel registerModel = new()
+        {
+            FirstName = "Juanpi11111",
+            LastName = "Taladro",
+            Email = "jt@mail.com",
+            Password = "Pass1234!"
+        };
+        public UserControllerTest()
+        {
+            var userBusiness = new UserBusiness(_unitOfWork, _mapper);
+            // Create user controller
+            usersController = new UsersController(userBusiness)
+            {
+                ControllerContext = _controllerContext
+            };
+        }
+        [Fact]
+        public async void Register_New_Ok()
+        {
+            //Act        
+            var result = await usersController.Register(registerModel);
+            //Assert
+            Assert.IsType<StatusCodeResult>(result);
+            var statusCodeResult = (StatusCodeResult)result;
+            // Test correct status code
+            Assert.Equal(201, statusCodeResult.StatusCode);
+            // Test user creation
+            Assert.NotNull(_unitOfWork.Users.GetById(1));
+            // Test accounts creation
+            Assert.NotNull(_unitOfWork.Accounts.GetById(1));
+            Assert.NotNull(_unitOfWork.Accounts.GetById(2));
+        }
+
+        [Fact]
+        public async void Register_Duplicated_Error()
+        {
+            //Act
+            await usersController.Register(registerModel);
+            //Try register same user
+            Func<Task> result = () => usersController.Register(registerModel);
+            //Assert
+            var exception = await Assert.ThrowsAsync<CustomException>(result);
+            Assert.Equal(400, exception.StatusCode);
+            Assert.Equal("Usuario ya registrado", exception.Error);
+        }
+    }
+}
+
