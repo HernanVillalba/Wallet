7b0e8569ef338fef848af3ce2b1ace90e5b08168
hernanVillalba <63215609+hernanVillalba@users.noreply.github.com>
2021-04-19 00:46:50 -0300

    create refund request in dollars and editable field in deatils to null (resolved)

--- Wallet/Wallet.Entities/TransactionLogModel.cs
+++ Wallet/Wallet.Entities/TransactionLogModel.cs
@@ -6,8 +6,6 @@ namespace Wallet.Entities
 {
     public class TransactionLogModel
     {
-        public int Id { get; set; }
-        public int TransactionId { get; set; }
         public string NewValue { get; set; }
         public DateTime ModificationDate { get; set; }
     }
